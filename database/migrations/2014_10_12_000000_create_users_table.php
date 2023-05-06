1f78505fa315a8f9fed58d48eab5dc2316852880
Yoga Andrian <yogaandrianp21@gmail.com>
2023-05-06 22:15:37 +0700

    database migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,22 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->bigIncrements('id');
+            $table->string('jenis_kandidat');
+            $table->string('nim');
+            $table->string('nama');
+            $table->string('jenjang');
+            $table->string('jurusan');
+            $table->string('program_studi');
+            $table->string('foto');
+            $table->string('ijazah');
+            $table->string('no_telp');
+            $table->string('url_linkedin');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('status');
             $table->timestamps();
         });
     }
---
+++ database/migrations/2023_05_06_125720_create_answers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreignId('id_pertanyaan');
+            $table->string('jawaban');
+            $table->integer('bobot');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
