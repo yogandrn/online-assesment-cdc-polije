33f773031e55d00ab3262532793d8d40781188b3
Deni Catur Kusumawati <75110391+denica123@users.noreply.github.com>
2023-05-11 14:22:45 +0700

    erorr sek

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\KepribadianController;
+use App\Http\Controllers\landingpage\HomeController;
+use App\Http\Controllers\Users\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,11 +17,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
 
 Route::name('admin.')->prefix('admin')->group(function () {
     Route::get('/', [DashboardController::class, 'index']);
     Route::get('/kepribadian', [KepribadianController::class, 'index']);
 });
+
+
+Route::name('users.')->prefix('users')->group(function () {
+    Route::get('/', [LoginController::class, 'index']);
+});
+
